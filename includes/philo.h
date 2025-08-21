949b3a439cdac06734942ac3b2771b4b703610da
miouz <mizhouis@gmail.com>
2025-08-21 18:47:21 +0200

    feature(utils.c): add helper functions

--- includes/philo.h
+++ includes/philo.h
@@ -18,12 +18,16 @@
 # include <pthread.h>
 # include <unistd.h>
 # include <stdbool.h>
+# include <string.h>
 
-// ERROR MESSAGES
-# define ARGS_ERROR "need 4 or 5 NUMERIC arguments: [number_of_philosophers]\
-	[time_to_die] [time_to_eat] [time_to_sleep]\
-	[number_of_times_each_philosopher_must_eat]\n"
+/*===============================ERROR MESSAGES===============================*/
+# define ARGS_ERROR "Error: need 4 or 5 NUMERIC arguments: [number_of_philosophers]\
+ [time_to_die] [time_to_eat] [time_to_sleep]\
+ [number_of_times_each_philosopher_must_eat]\n"
+# define ARGS_ERROR_NOPHILO "Error: need at least 1 philo -.-\n"
+# define MALLOC_ERROR "Error: Malloc failed\n"
 
+/*===============================STRUCTS======================================*/
 typedef struct s_philo
 {
 	int				*status;
@@ -36,6 +40,16 @@ typedef struct s_philo
 	int				time_to_eat;
 }	t_philo;
 
-int	ft_strlen(char *str);
-int	error_quit(char *msg);
+/*===============================PARSING======================================*/
+bool		is_valid_args(int argc, char **argv);
+
+/*===============================INIT======================================*/
+int			init_philos(t_philo *philo, char **argv);
+
+/*===============================UTILS======================================*/
+long int	ft_atol(const char *nptr);
+int			ft_strlen(char *str);
+void		free_philos(t_philo *philos);
+int			error_quit(char *msg);
+
 #endif // !PHILO_H
