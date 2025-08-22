2cedb30c71ba64ae9eef43e0d9d4575153502cf0
miouz <mizhouis@gmail.com>
2025-08-23 01:27:33 +0200

    feature(routine): add routines not complete

--- srcs/routine.c
+++ srcs/routine.c
@@ -14,6 +14,53 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include <sys/time.h>
+#include <unistd.h>
+
+static int	routine_sleep(t_philo *philo, int id)
+{
+	pthread_mutex_lock(&philo->print_mutex);
+	printf("philo[%d] begin to sleep\n", id);
+	pthread_mutex_unlock(&philo->print_mutex);
+	sleep(philo->time_to_sleep);
+	return (EXIT_SUCCESS);
+}
+
+static int	routine_think(t_philo *philo, int id)
+{
+	pthread_mutex_lock(&philo->print_mutex);
+	printf("philo[%d] begin to think\n", id);
+	pthread_mutex_unlock(&philo->print_mutex);
+	return (EXIT_SUCCESS);
+}
+
+static int	pick_fork(t_philo *philo, int id_fork)
+{
+	while (1)
+	{
+		if (philo->forks[id_fork - 1] == FREE_TO_USE)
+			break ;
+	}
+	pthread_mutex_lock(&philo->fork_mutex[id_fork - 1]);
+	printf("philo[%d] take a fork\n", id);
+	pthread_mutex_unlock(&philo->print_mutex);
+	return (EXIT_SUCCESS);
+
+}
+
+static int	routine_eat(t_philo *philo, int id)
+{
+	if (id == philo->philo_num - 1)
+	{
+		pick_fork(philo, id - 1);
+		pick_fork(philo, id);
+	}
+	else
+	{
+		pick_fork(philo, id);
+		pick_fork(philo, id - 1);
+	}
+	return (EXIT_SUCCESS);
+}
 
 /**
  * @brief The fucntion of thread.
@@ -21,12 +68,18 @@
  * @param philos The philos structure.
  * @return SUCCESS or FAILURE.
  */
-static void	*thread_routine(void *philos)
+static void	*thread_routine(void *philo)
 {
-	printf("my id is philo %d\n", ((t_philo *)philos)->philo_id);
-	// routine_eat(philos);
-	// routine_sleep(philos);
-	// routine_think(philos);
+	int	id;
+
+	pthread_mutex_lock(&(((t_philo *)philo)->id_mutex));
+	((t_philo *)philo)->philo_id++;
+	id = ((t_philo *)philo)->philo_id;
+	pthread_mutex_unlock(&(((t_philo *)philo)->id_mutex));
+	printf("my id is philo %d\n", id);
+	routine_eat(philo, id);
+	routine_sleep(philo, id);
+	routine_think(philo, id);
 	return (EXIT_SUCCESS);
 }
 
@@ -50,7 +103,6 @@ int	start_routine(t_philo *philos)
 	n = 0;
 	while (n < philos->philo_num)
 	{
-		philos->philo_id = n + 1;
 		ret = pthread_create(&philos->thread_ids[n], NULL,
 				thread_routine, (void *)philos);
 		if (ret < 0)
