5b289a92b2b2482d8f7f70e23e07a12295ee8541
Mi Zhou <mzhou@student.42.fr>
2025-08-26 15:39:59 +0200

    feature(eat_routine):add eat routine, pick fork and drop fork

--- srcs/routine_eat.c
+++ srcs/routine_eat.c
@@ -11,39 +11,112 @@
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
+#include <stdlib.h>
+#include <unistd.h>
 
-// static int	pick_fork(t_philo *philo, int id_fork, int id_philo)
-// {
-// 	int	index_fork;
-//
-// 	index_fork = id_fork - 1;
-// 	while (1)
-// 	{
-// 		if (philo->forks[index_fork] == FREE_TO_USE)
-// 			break ;
-// 	}
-// 	pthread_mutex_lock(&philo->fork_mutex[index_fork]);
-// 	philo->forks[id_fork - 1] = OCCUPIED;
-// 	pthread_mutex_unlock(&philo->fork_mutex[index_fork]);
-// 	pthread_mutex_lock(&philo->print_mutex);
-// 	printf("philo[%d] has taken a fork[%d]\n", id_philo, id_fork);
-// 	pthread_mutex_unlock(&philo->print_mutex);
-// 	return (EXIT_SUCCESS);
-// }
+static int	drop_fork(t_philo *philo, int id_fork, int id_philo)
+{
+	int	retval;
+
+	retval = EXIT_FAILURE;
+	while (1)
+	{
+		retval = pthread_mutex_unlock(&philo->fork_mutex[id_fork]);
+		if (retval == EXIT_SUCCESS)
+			break ;
+	}
+	print_status(philo, id_philo, "has drop a fork\n");
+	return (EXIT_SUCCESS);
+}
+
+static int	pick_fork(t_philo *philo, int id_fork, int id_philo)
+{
+	int	retval;
+
+	retval = EXIT_FAILURE;
+	while (1)
+	{
+		retval = pthread_mutex_lock(&philo->fork_mutex[id_fork]);
+		if (retval == EXIT_SUCCESS)
+			break ;
+	}
+	print_status(philo, id_philo, "has taken a fork\n");
+	return (EXIT_SUCCESS);
+}
+
+static int	drop_two_forks(t_philo *philo, int id)
+{
+	int	left_fork;
+	int	right_fork;
+
+	left_fork = id;
+	if (id == 0)
+		right_fork = philo->philo_num - 1;
+	else
+		right_fork = id - 1;
+	if (id == philo->philo_num - 1)
+	{
+		if (drop_fork(philo, left_fork, id) == EXIT_SUCCESS
+			&& drop_fork(philo, right_fork, id) == EXIT_SUCCESS)
+			return (EXIT_SUCCESS);
+	}
+	else
+	{
+		if (drop_fork(philo, right_fork, id) == EXIT_SUCCESS
+			&& drop_fork(philo, left_fork, id) == EXIT_SUCCESS)
+			return (EXIT_SUCCESS);
+	}
+	return (EXIT_FAILURE);
+}
+
+/**
+ * @brief The eat routine
+ * @detail
+ * 	-Take one fork
+ *	-Take another fork
+ *	-Begin to eat
+ *	-Drop one fork
+ *	-Drop another fork
+ * @param philo [The philo structure]
+ * @param id Philo's id
+ * @return EXIT_SUCCESS or EXIT_FAILURE
+ */
+static int	get_two_forks(t_philo *philo, int id)
+{
+	int	left_fork;
+	int	right_fork;
+
+	left_fork = id;
+	if (id == 0)
+		right_fork = philo->philo_num - 1;
+	else
+		right_fork = id - 1;
+	if (id == philo->philo_num - 1)
+	{
+		if (pick_fork(philo, left_fork, id) == EXIT_SUCCESS
+			&& pick_fork(philo, right_fork, id) == EXIT_SUCCESS)
+			return (EXIT_SUCCESS);
+	}
+	else
+	{
+		if (pick_fork(philo, right_fork, id) == EXIT_SUCCESS
+			&& pick_fork(philo, left_fork, id) == EXIT_SUCCESS)
+			return (EXIT_SUCCESS);
+	}
+	return (EXIT_SUCCESS);
+}
 
 int	routine_eat(t_philo *philo, int id)
 {
-	(void)id;
-	usleep(philo->time_to_eat);
-	// if (id == philo->philo_num - 1)
-	// {
-	// 	pick_fork(philo, id - 1, id);
-	// 	pick_fork(philo, id, id);
-	// }
-	// else
-	// {
-	// 	pick_fork(philo, id, id);
-	// 	pick_fork(philo, id - 1, id);
-	// }
-	return (EXIT_SUCCESS);
+	if (id == 5)
+		stop_simulation(philo);
+	if (get_two_forks(philo, id) == EXIT_SUCCESS)
+	{
+		print_status(philo, id, EAT);
+		usleep(philo->time_to_eat);
+		if (drop_two_forks(philo, id) == EXIT_SUCCESS)
+			return (EXIT_SUCCESS);
+	}
+	stop_simulation(philo);
+	return (EXIT_FAILURE);
 }
