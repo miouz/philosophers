b506b9b291426926506fde1d8fe43cdc6e29d7a5
Mi Zhou <mzhou@student.42.fr>
2025-08-26 15:41:52 +0200

    feature(main): add terminate terminate threads function to wait all threads to join
    it checks if thread id is not 0

--- srcs/main.c
+++ srcs/main.c
@@ -11,7 +11,22 @@
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
+#include <pthread.h>
 #include <stdlib.h>
+
+int	terminate_threads(t_philo *philos)
+{
+	int			n_threads;
+
+	n_threads = 0;
+	while (n_threads < philos->philo_num - 1
+		&& philos->thread_ids[n_threads] != 0)
+	{
+		pthread_join(philos->thread_ids[n_threads], NULL);
+		n_threads++;
+	}
+	return (EXIT_SUCCESS);
+}
 
 int	main(int argc, char **argv)
 {
@@ -21,8 +36,8 @@ int	main(int argc, char **argv)
 		return (error_msg(ARGS_ERROR), EXIT_FAILURE);
 	if (init_philos(&philos, &argv[1]) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
-	if (start_routine(&philos) == EXIT_FAILURE)
-		return (free_philos(&philos), EXIT_FAILURE);
+	start_routine(&philos);
+	terminate_threads(&philos);
 	free_philos(&philos);
 	return (EXIT_SUCCESS);
 }
