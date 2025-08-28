867d2fc6d2c50aed13c4c9ac3093e224de5137c9
miouz <mizhouis@gmail.com>
2025-08-28 23:38:38 +0200

    fix(routien_eat): fix eat actions with right structure type

--- srcs/routine_eat.c
+++ srcs/routine_eat.c
@@ -12,14 +12,14 @@
 
 #include "../includes/philo.h"
 
-static int	drop_fork(t_philo *philo, int id_fork, int id_philo)
+static int	drop_fork(t_philo *philo, pthread_mutex_t *fork_mutex, int id_philo)
 {
 	int	retval;
 
 	retval = EXIT_FAILURE;
 	while (1)
 	{
-		retval = pthread_mutex_unlock(&philo->fork_mutex[id_fork]);
+		retval = pthread_mutex_unlock(fork_mutex);
 		if (retval == EXIT_SUCCESS)
 			break ;
 	}
@@ -27,14 +27,14 @@ static int	drop_fork(t_philo *philo, int id_fork, int id_philo)
 	return (EXIT_SUCCESS);
 }
 
-static int	pick_fork(t_philo *philo, int id_fork, int id_philo)
+static int	pick_fork(t_philo *philo, pthread_mutex_t *fork_mutex, int id_philo)
 {
 	int	retval;
 
 	retval = EXIT_FAILURE;
 	while (1)
 	{
-		retval = pthread_mutex_lock(&philo->fork_mutex[id_fork]);
+		retval = pthread_mutex_lock(fork_mutex);
 		if (retval == EXIT_SUCCESS)
 			break ;
 	}
@@ -44,15 +44,15 @@ static int	pick_fork(t_philo *philo, int id_fork, int id_philo)
 
 static int	drop_two_forks(t_philo *philo, int id)
 {
-	int	left_fork;
-	int	right_fork;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*right_fork;
 
-	left_fork = id;
+	left_fork = &philo->fork_mutex;
 	if (id == 0)
-		right_fork = philo->philo_num - 1;
+		right_fork = &(philo + (philo->prog_data->philo_num - 1))->fork_mutex;
 	else
-		right_fork = id - 1;
-	if (id == philo->philo_num - 1)
+		right_fork = &(philo - 1)->fork_mutex;
+	if (id == philo->prog_data->philo_num - 1)
 	{
 		if (drop_fork(philo, left_fork, id) == EXIT_SUCCESS
 			&& drop_fork(philo, right_fork, id) == EXIT_SUCCESS)
@@ -81,35 +81,37 @@ static int	drop_two_forks(t_philo *philo, int id)
  */
 static int	get_two_forks(t_philo *philo, int id)
 {
-	int	left_fork;
-	int	right_fork;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*right_fork;
 
-	left_fork = id;
+	left_fork = &philo->fork_mutex;
 	if (id == 0)
-		right_fork = philo->philo_num - 1;
+		right_fork = &(philo + (philo->prog_data->philo_num - 1))->fork_mutex;
 	else
-		right_fork = id - 1;
-	if (id == philo->philo_num - 1)
+		right_fork = &(philo - 1)->fork_mutex;
+	if (id == philo->prog_data->philo_num - 1)
 	{
 		if (pick_fork(philo, left_fork, id) == EXIT_SUCCESS
+			&& should_stop_simulation(philo) == false
 			&& pick_fork(philo, right_fork, id) == EXIT_SUCCESS)
 			return (EXIT_SUCCESS);
 	}
 	else
 	{
 		if (pick_fork(philo, right_fork, id) == EXIT_SUCCESS
+			&& should_stop_simulation(philo) == false
 			&& pick_fork(philo, left_fork, id) == EXIT_SUCCESS)
 			return (EXIT_SUCCESS);
 	}
 	return (EXIT_SUCCESS);
 }
 
-int	update_last_meal_time_and_times_eaten(t_philo *philo, int id)
+int	update_last_meal_time_and_times_eaten(t_philo *philo)
 {
-	pthread_mutex_lock(&philo->last_meal_time_and_times_eaten_mutex[id]);
-	gettimeofday(&philo->last_meal_time[id], NULL);
-	philo->times_eaten[id]++;
-	pthread_mutex_unlock(&philo->last_meal_time_and_times_eaten_mutex[id]);
+	pthread_mutex_lock(&philo->last_meal_time_and_times_eaten_mutex);
+	gettimeofday(&philo->last_meal_time, NULL);
+	philo->times_eaten++;
+	pthread_mutex_unlock(&philo->last_meal_time_and_times_eaten_mutex);
 	return (EXIT_SUCCESS);
 }
 
@@ -118,9 +120,9 @@ int	routine_eat(t_philo *philo)
 	if (get_two_forks(philo, philo->philo_id) == EXIT_SUCCESS)
 	{
 		print_status(philo, philo->philo_id, EAT);
-		update_last_meal_time_and_times_eaten(philo, philo->philo_id);
+		update_last_meal_time_and_times_eaten(philo);
 		segments_usleep(philo, philo->prog_data->time_to_eat);
-		if (drop_two_forks(philo, id) == EXIT_SUCCESS)
+		if (drop_two_forks(philo, philo->philo_id) == EXIT_SUCCESS)
 			return (EXIT_SUCCESS);
 	}
 	stop_simulation(philo);
